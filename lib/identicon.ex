aecfe270d9e32e1500bb5b4dab871846c14fb8c5
severejetlag <nhl1013@gmail.com>
2018-11-06 10:27:31 -0800

    Updating from previous day. Added filter odd grid values

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -21,6 +21,15 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _inded}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -28,9 +37,14 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
